551e6a7ff23acd581872dd5f025a92559aee1c96
FreyaTaylor <1214519210@qq.com>
2023-11-08 23:01:40 +0800

    test main

---
+++ Main1.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
+
+}
